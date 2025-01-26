1bd4199525aa166ad0ba39cc23aa1eabed2df898
tsuhani-19 <tsuhani1234@gmail.com>
2025-01-26 19:59:25 +0530

    Initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
